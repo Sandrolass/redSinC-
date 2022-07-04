926e8ed01acbc152e4a83f1ce05f8d9189486d7e
Sandro Lopez <sandro.informaticat@gmail.com>
2022-07-04 22:58:27 +0200

    comentarios añadidos al comienzo

--- main.cpp
+++ main.cpp
@@ -8,7 +8,9 @@
 //typedef ap_fixed<32,8,AP_RND,AP_SAT> apfixed;
 using namespace std;
 
-//--
+//-- Este cpp solo sirve para realizar la operacion de la red de python de otro repositorio anterior
+// data.txt contiene los pesos y bias de la red neuronal antes mencionada , cambiando los tamaños de los arrays podemos ser capaces de realizar
+// los procesos de cualquier red de 4 capas , ( para más capas deberían hacerse los suso dichas funciones)
 
 
 
@@ -112,13 +114,6 @@ double biasI[6],double biash1[5], double biash2[4]){
 }
 
 
-/*  double void  red(){
-    forwardInput(inputValsI,outputValsI,inputLayer,bias1);
-    forwardh1(outputValsI,outputValsH1,hiddenLayer1,bias2);
-    forwardh2(outputValsH1,outputValsH2,hiddenLayer2,bias3);
-   return  res = forwardOut(outputValsH2,output,outputLayer,bias4);
-} */ 
-
 double runAll(double in , double inputWeights[6],double bias1[6],double hiddenLayer1[5][6],double bias2[5],
             double hiddenLayer2[4][5],double bias3[4],double outputLayer[4], double bias4 ){
                 double outputValsI[6],outputValsH1[5],outputValsH2[4];
@@ -134,26 +129,13 @@ double runAll(double in , double inputWeights[6],double bias1[6],double hiddenLa
 
 int main(){
     
-    double bias1[6]={-1.0661598,0.0,0.0,0.0,0.0,0.10200185};
-    double bias2[5]={-1.8157208,0.82531506,0.78921926,0.44062296,0.7459145};
-    double bias3[4]={-1.6017522,1.4491416,0.0,-0.03135741};
-    double bias4 =-1.3076416;
+    double bias1[6],bias2[5],bias3[4],bias4;
     double inputLayer[6], hiddenLayer1[5][6],hiddenLayer2[4][5],outputLayer[4];
     bias4 = loadData(inputLayer,hiddenLayer1,hiddenLayer2,outputLayer,bias1,bias2,bias3);
     double res;
     string cadena;
     ifstream newfile("x.txt");
     ofstream secondfile("res.txt");
-    /*for(int i=0;i<1000;i++){
-        newfile>>cadena;
-        forwardInput(stod(cadena),outputValsI,inputLayer,bias1);
-        forwardh1(outputValsI,outputValsH1,hiddenLayer1,bias2);
-        forwardh2(outputValsH1,outputValsH2,hiddenLayer2,bias3);
-        res = forwardOut(outputValsH2,output,outputLayer,bias4);
-        secondfile << res;
-        secondfile << endl;
-        cout<<res*2-1<<endl;
-    }*/
    for(int i=0;i<1000;i++){
        newfile>>cadena;
        res =  runAll(stod(cadena),inputLayer,bias1,hiddenLayer1,bias2,hiddenLayer2,bias3,outputLayer,bias4);
